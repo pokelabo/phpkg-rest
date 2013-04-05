2d82c6560dba215f67fb85f0359de0f03453aa70
aichikawa <aichikawa@192.168.10.57>
2013-04-05 14:56:45 +0900

    exampleから不要のコードを削除

--- example/app/bootstrap.php
+++ example/app/bootstrap.php
@@ -9,19 +9,3 @@ $loader->register(true);
 
 use pokelabo\config\ConfigRepository;
 ConfigRepository::addConfigDir(realpath(__DIR__ . '/config'));
-
-function createRedisClient() {
-    $config = ConfigRepository::load('redis');
-    $redis = new Redis();
-    $timeout = 2.5;
-    $redis->pconnect($config->dig('host'), $config->dig('port'), $config->dig('timeout'));
-}
-
-function getGlobalConfig() {
-    static $config = null;
-    if ($config === null) {
-        $etc_dir = realpath(dirname(dirname(__DIR__)) . '/etc');
-        $config = json_decode(file_get_contents($etc_dir . '/config.json'), true);
-    }
-    return $config;
-}
